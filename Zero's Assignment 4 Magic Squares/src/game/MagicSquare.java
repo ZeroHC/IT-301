63601454b393f012d4933807430c11c74c8c116c
ZeroHC <772185085@qq.com>
2017-10-27 10:32:36 -0700

    Basic Idea about this program

---
+++ Zero's Assignment 4 Magic Squares/src/game/MagicSquare.java
@@ -0,0 +1,31 @@
+package game;
+
+public class MagicSquare
+{
+    private short choices;
+
+    public MagicSquare()
+    {
+        this.choices = choices;
+    }
+
+    public boolean choose(byte selection)
+    {
+        return false;
+    }
+
+    public boolean hasAlreadyChosen(byte selection)
+    {
+        return false;
+    }
+
+    public short getChoices()
+    {
+        return choices;
+    }
+
+    public String printChoices()
+    {
+        return null;
+    }
+}
