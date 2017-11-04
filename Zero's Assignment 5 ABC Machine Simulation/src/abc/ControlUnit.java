d0744b67390348d973eaa5cd8283917ca7b68692
Hanchen Liu <772185085@qq.com>
2017-11-03 19:14:31 -0700

    fetch and decode part finished

--- Zero's Assignment 5 ABC Machine Simulation/src/abc/ControlUnit.java
+++ Zero's Assignment 5 ABC Machine Simulation/src/abc/ControlUnit.java
@@ -4,8 +4,15 @@ import abc.ABCMachine;
 
 public class ControlUnit
 {
+    //masks for decoding instruction code
+    private static final short OP_CODE_MASK = -8192;            //111 000 000 000 0000
+    private static final short SOURCE_REGISTER_1_MASK = 7168;   //000 111 000 000 0000
+    private static final short SOURCE_REGISTER_2_MASK = 896;    //000 000 111 000 0000
+    private static final short DESTINATION_REGISTER_MASK = 112; //000 000 000 111 0000
+    private static final short ADDRESS_MASK = 15;               //000 000 000 000 1111
+
 	private ABCMachine machine;
-	
+
 	public ControlUnit(ABCMachine machine)
 	{
 		this.machine = machine;
@@ -20,40 +27,52 @@ public class ControlUnit
          * "halt" when it reaches an instruction that is
          * zero in binary.
          */
-        while (!(machine.getIr() == 0))
+
+
+        short instruction;
+        short[] instructionParts;
+
+        while (true)
         {
-            fetch();
+            instruction = fetch();
+            if (instruction == 0)
+            {
+                break;
+            }
+
+            else
+            {
+                instructionParts = decode(instruction);
+
+                execute(instructionParts);
+            }
         }
 	}
 
-	private void fetch()
+	//this method fetches thur the register and memory and returns one instruction code
+	private short fetch()
     {
-        for (short register : machine.getRegisters())
-        {
-            byte address = 0;
-            for (short memory : machine.getMemory())
-            {
-                if (register == memory)
-                {
-                    for (short memoryAddress: machine.getMemory())
-                    {
-                        if ((memoryAddress & address) == address)
-                        {
-                            machine.setIr(memoryAddress);
-                        }
-                    }
-                }
-                address++;
-            }
-        }
+        byte pc = machine.getPc();
+        machine.setIr(machine.getMemory()[pc]);
+        machine.setPc((byte)(pc + 1));
+
+        return machine.getIr();
     }
 
-    private void decode()
+    //this method decodes the instruction code into 5 sub parts
+    private short[] decode(short instruction)
     {
+        short opCode = (short)(instruction & OP_CODE_MASK);
+        short sourceRegister1 = (short)(instruction & SOURCE_REGISTER_1_MASK);
+        short sourceRegister2 = (short)(instruction & SOURCE_REGISTER_2_MASK);
+        short destinationRegister = (short)(instruction & DESTINATION_REGISTER_MASK);
+        short address = (short)(instruction & ADDRESS_MASK);
 
+        return new short[]{opCode, sourceRegister1, sourceRegister2, destinationRegister, address};
     }
 
-    private void execute()
+    //this method executes the instruction code
+    private void execute(short[] instructionCodes)
     {
 
     }
